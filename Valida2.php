ec3522555b271ee56d2f15ac5b1c80a8fb8798d4
JuanPortela <75047873+JuanPortela@users.noreply.github.com>
2021-11-16 17:19:57 -0300

    Add files via upload

--- Valida2.php
+++ Valida2.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-include_once("conexao1.php");
+include_once("conexao2.php");
 if((isset($_POST['email'])) && (isset($_POST['senha']))) {	
 	$usuario =mysqli_real_escape_string($conn, $_POST['email']);
 	$senha =mysqli_real_escape_string($conn, $_POST['senha']);
     
 
-    $sql = "SELECT * FROM clientes WHERE email= '$usuario' && senha = '$senha' LIMIT 1";
+    $sql = "SELECT * FROM funcionario WHERE email= '$usuario' && senha = '$senha' LIMIT 1";
     $result = mysqli_query($conn, $sql);
 	$resultado = mysqli_fetch_assoc($result);
 	
